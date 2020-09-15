af28ee45c0dd06ad0795c119190d4a9605e16709
Marina8989 <marina.mill209@gmail.com>
2020-09-15 14:02:09 -0700

    drum kit

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 window.addEventListener('keydown', function(e) {
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   console.log(audio)
+   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+   console.log(key)
    //if there is no audio for key, stop from running all together
    if(!audio) return;
+   //rewind the soud to the start right away
+   audio.currentTime = 0;
+   audio.play();
+   key.classList.add('playing');
 })
 
+function removeTransition() {
+   return this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 
