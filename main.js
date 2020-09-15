d859951061e6b2ae1f47d16d5b75a61f0937f564
Marina8989 <marina.mill209@gmail.com>
2020-09-15 14:55:49 -0700

    drum kit

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
+// function playSound(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+//     //if there is no audio for key, stop from running all together
+//     if (!audio) return;
+//     //rewind the soud to the start right away
+//     audio.currentTime = 0;
+//     audio.play();
+//     key.classList.add('playing');
+// }
+
+// function removeTransition(e) {
+//     //if it is not a transform, skip it
+//     if(e.propertyName !== 'transform') return;
+//     //otherwise remove the property
+//     this.classList.remove('playing');
+// }
+
+// const keys = document.querySelectorAll('.key');
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// window.addEventListener('keydown', playSound);
+
+
+
+
+
+
+
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    
-    //if there is no audio for key, stop from running all together
+    console.log(audio)
+    console.log(key)
+
     if (!audio) return;
-    //rewind the soud to the start right away
     audio.currentTime = 0;
     audio.play();
+
     key.classList.add('playing');
 }
 
 function removeTransition(e) {
-    //if it is not a transform, skip it
-    if(e.propertyName !== 'transform') return;
-    //otherwise remove the property
-    this.classList.remove('playing');
+     if(e.propertyName !== 'transform') return;
+     this.classList.remove('playing');
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
