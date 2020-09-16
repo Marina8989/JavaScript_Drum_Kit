f73c79b0b3b71d57b66dd9dbbff9a2b45a934014
Marina8989 <marina.mill209@gmail.com>
2020-09-15 21:03:10 -0700

    drum kit

--- main.js
+++ main.js
@@ -2,21 +2,25 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    console.log(audio)
-    console.log(key)
-
+   
+    //if the adio doesn't exist, exit 
     if (!audio) return;
+    //otherwise play audio and mke sure it starts from beginning every time user clickes the key
     audio.currentTime = 0;
     audio.play();
-
+    //add the class to key
     key.classList.add('playing');
 }
 
 function removeTransition(e) {
+    //if key doesn't have a property (in CSS), return right away
      if(e.propertyName !== 'transform') return;
+    // otherwise remove that class 
      this.classList.remove('playing');
 }
-
+//select all keys
 const keys = document.querySelectorAll('.key');
+//loop through them and add event
 keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+//to the window add event listener on key dowm and callback function
 window.addEventListener('keydown', playSound)
